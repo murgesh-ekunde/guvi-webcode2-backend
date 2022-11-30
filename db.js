150344c1cc0cace2a872e7d58ba0eddb83494b77
Murgesh Ekunde <mekunde@gmail.com>
2022-11-30 20:24:00 +0530

    fixed isseus

--- db.js
+++ db.js
@@ -3,28 +3,33 @@ const {MongoClient} = require('mongodb');
 require ('dotenv').config();
 
 // Database connection
-module.exports = () =>{
-    selectedDb: {}
+module.exports = () => {
+	const connectionParams = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	};
+	try {
+		mongoose.connect(process.env.DB, connectionParams);
+		console.log("Connected to database successfully");
+	} catch (error) {
+		console.log(error);
+		console.log("Could not connect database!");
+	}
+};
 
-    const connectionParams= 
-    {useNewUrlParser: true,
-    useUnifiedTopology:true,};
 
-    async function connect(){
-        try {
-            const client = new MongoClient.connect(process.env.DB, connectionParams);
-            await client.connect();
-            this.selectedDb = client.db('bms');
-        } catch(err) {
-            console.error(err);
-        }}
+// function connectToDB (){
+//     selectedDb: {}
+//     const connectionParams= 
+//     {useNewUrlParser: true,
+//     useUnifiedTopology:true,};
 
-    try {
-        mongoose.connect(process.env.DB, connectionParams)
-        console.log("Connected to database successfully")
-    } catch (error) {
-        console.log(error);
-        console.log("Could not connect to database!")
-    }
-}
-
+//     try {
+//         const connection  = mongoose.connect(process.env.DB, connectionParams)
+//         console.log("Connected to database successfully")
+//         this.selectedDb = client.db('bms');
+//     } catch (error) {
+//         console.log(error);
+//         console.log("Could not connect to database!")
+//     }
+// }
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express() 
 const port= process.env.PORT || 3000
 const cors = require("cors")
-const connection = require("./db")
+const connection = require("./db");
 const userRoutes = require("./routes/users")
 const authRoutes = require("./routes/auth")
 
